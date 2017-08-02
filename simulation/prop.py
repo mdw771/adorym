21b4b8254c4bbbb55e0cbd4f1cba9ee32f2701eb
Ming <mingdu2015@u.northwestern.edu>
2017-08-01 20:20:30 -0500

    1ft

--- simulation/prop.py
+++ simulation/prop.py
@@ -39,10 +39,10 @@ def propIR(u1,step,L,wavel,z) :
     x = np.linspace(-L/2.0,L/2.0-step,M)
     y = np.linspace(-L/2.0,L/2.0-step,N)
     X,Y = np.meshgrid(x,y)
-    h = (1/(1j*wavel*z))*np.exp(1j*k*(1/(2*z))*(X**2+Y**2))
+    h = (np.exp(1j*k*z)/(1j*wavel*z))*np.exp(1j*k*(1/(2*z))*(X**2+Y**2))
     H = fft.fft2(fft.fftshift(h))*step*step
     U1 = fft.fft2(fft.fftshift(u1))
-    U2 = np.multiply((H),(U1))
+    U2 = H * U1
     u2 = fft.ifftshift(fft.ifft2(U2))
     return u2
 '''
@@ -59,4 +59,21 @@ def propFF(u1,step,L1,wavel,z):
     X2,Y2 = np.meshgrid(x2,x2)
     c = 1/(1j*wavel*z)*np.exp(((1j*k)/(2*z))*(X2**2+Y2**2))
     u2 = np.multiply(c,fft.ifftshift(fft.fft2(fft.fftshift(u1))))*step*step
+    return u2,L2
+
+def prop1FT(u1,step,L1,wavel,z):
+    M,N = np.shape(u1)
+    k = 2*np.pi/wavel
+    print(wavel,z,step)
+    x = np.linspace(-L1/2.0,L1/2.0-step,M)
+    y = np.linspace(-L1/2.0,L1/2.0-step,N)
+    X,Y = np.meshgrid(x,y)
+    L2 = wavel*z/step
+    step2 = wavel*z/L1
+    n = L2/step2 #number of samples
+    x2 = np.linspace(-L2/2.0,L2/2.0,n)
+    X2,Y2 = np.meshgrid(x2,x2)
+    c = 1/(1j*wavel*z)*np.exp(((1j*k)/(2*z))*(X2**2+Y2**2))
+    c0 = np.exp(1j * k / (2 * z) * (X**2 + Y**2))
+    u2 = np.multiply(c,fft.ifftshift(fft.fft2(fft.fftshift(u1*c0))))*step*step
     return u2,L2
