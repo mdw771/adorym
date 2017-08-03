068ff382867f418633e7b898541cc4d2cd644f5c
Ming <mingdu2015@u.northwestern.edu>
2017-08-02 20:26:08 -0500

    fix bug cast z

--- simulation/prop.py
+++ simulation/prop.py
@@ -25,21 +25,24 @@ def propTF(u1,step,L,wavel,z) :
     fx = fft.fftfreq(M,d=step)
     fy = fft.fftfreq(N,d=step)
     FX,FY = np.meshgrid((fx),(fy))
+    FX = fft.fftshift(FX)
+    FY = fft.fftshift(FY)
+    print FX
     H = np.exp(-1j*np.pi*wavel*z*(FX**2+FY**2))
-    U1 = fft.fft2(u1)
+    U1 = fft.fftshift(fft.fft2(u1))
     U2 = np.multiply(H,(U1))
-    u2 = fft.ifft2((U2))
+    u2 = fft.ifft2(fft.ifftshift(U2))
     return u2
 '''
 Propogation using the Impulse Response function. The convention of shiftinng a function in realspace before performing the fourier transform which is used in the reference is followed here. Input convention as above
 '''
-def propIR(u1,step,L,wavel,z) :
+def propIR(u1,step,L,wavel,z):
     M,N = np.shape(u1)
     k = 2*np.pi/wavel
     x = np.linspace(-L/2.0,L/2.0-step,M)
     y = np.linspace(-L/2.0,L/2.0-step,N)
     X,Y = np.meshgrid(x,y)
-    h = (np.exp(1j*k*z)/(1j*wavel*z))*np.exp(1j*k*(1/(2*z))*(X**2+Y**2))
+    h = (np.exp(1j*k*z)/(1j*wavel*z))*np.exp(1j*k*(1./(2*z))*(X**2+Y**2))
     H = fft.fft2(fft.fftshift(h))*step*step
     U1 = fft.fft2(fft.fftshift(u1))
     U2 = H * U1
@@ -73,7 +76,7 @@ def prop1FT(u1,step,L1,wavel,z):
     n = L2/step2 #number of samples
     x2 = np.linspace(-L2/2.0,L2/2.0,n)
     X2,Y2 = np.meshgrid(x2,x2)
-    c = 1/(1j*wavel*z)*np.exp(((1j*k)/(2*z))*(X2**2+Y2**2))
+    c = 1/(1j*wavel*z)*np.exp(((1j*k)/(2.*z))*(X2**2+Y2**2))
     c0 = np.exp(1j * k / (2 * z) * (X**2 + Y**2))
     u2 = np.multiply(c,fft.ifftshift(fft.fft2(fft.fftshift(u1*c0))))*step*step
     return u2,L2
