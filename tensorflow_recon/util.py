594d0d2e513768f8904990ebd00af71f17761bc2
Ming <mingdu2015@u.northwestern.edu>
2018-01-10 15:08:50 -0600

    diff

--- tensorflow_recon/util.py
+++ tensorflow_recon/util.py
@@ -1,16 +1,143 @@
 import tomopy
 import tensorflow as tf
 import numpy as np
+import dxchange
 import matplotlib.pyplot as plt
 try:
     from xdesign.propagation import get_kernel_tf_real, get_kernel
     from xdesign.constants import *
-    from xdesign.acquisition import Simulator
     import sys
     from scipy.ndimage import gaussian_filter
     from scipy.ndimage import fourier_shift
 except:
     pass
+import warnings
+import os
+
+
+class Simulator(object):
+    """Optical simulation based on multislice propagation.
+
+    Attributes
+    ----------
+    grid : numpy.ndarray or list of numpy.ndarray
+        Descretized grid for the phantom object. If type == 'refractive_index',
+        it takes a list of [delta_grid, beta_grid].
+    energy : float
+        Beam energy in eV. Should match the energy used for creating the grids.
+    psize : list
+        Pixel size in cm.
+    type : str
+        Value type of input grid.
+    """
+
+    def __init__(self, energy, grid=None, psize=None, type='refractive_index'):
+
+        if type == 'refractive_index':
+            if grid is not None:
+                self.grid_delta, self.grid_beta = grid
+            else:
+                self.grid_delta = self.grid_beta = None
+            self.energy_kev = energy * 1.e-3
+            self.voxel_nm = np.array(psize) * 1.e7
+            self.mean_voxel_nm = np.prod(self.voxel_nm)**(1. / 3)
+            self._ndim = 3
+            self.size_nm = np.array(self.grid_delta.get_shape().as_list()) * self.voxel_nm
+            self.shape = self.grid_delta.get_shape().as_list()
+            self.lmbda_nm = 1.24 / self.energy_kev
+            self.mesh = []
+            temp = []
+            for i in range(self._ndim):
+                temp.append(np.arange(self.shape[i]) * self.voxel_nm[i])
+            self.mesh = np.meshgrid(*temp, indexing='xy')
+
+            # wavefront in x-y plane or x edge
+            self.wavefront = np.zeros(self.grid_delta.shape[:-1], dtype=np.complex64)
+        else:
+            raise ValueError('Currently only delta and beta grids are supported.')
+
+    def save_grid(self, save_path='data/sav/grid'):
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        np.save(os.path.join(save_path, 'grid_delta'), self.grid_delta)
+        np.save(os.path.join(save_path, 'grid_beta'), self.grid_beta)
+        grid_pars = [self.shape, self.voxel_nm, self.energy_kev * 1.e3]
+        np.save(os.path.join(save_path, 'grid_pars'), grid_pars)
+
+    def read_grid(self, save_path='data/sav/grid'):
+        try:
+            self.grid_delta = np.load(os.path.join(save_path, 'grid_delta.npy'))
+            self.grid_beta = np.load(os.path.join(save_path, 'grid_beta.npy'))
+        except:
+            raise ValueError('Failed to read grid.')
+
+    def save_slice_images(self, save_path='data/sav/slices'):
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        dxchange.write_tiff_stack(self.grid_delta, os.path.join(save_path, 'delta'),
+                                  overwrite=True, dtype=np.float32)
+        dxchange.write_tiff_stack(self.grid_beta, os.path.join(save_path, 'beta'),
+                                  overwrite=True, dtype=np.float32)
+
+    def show_grid(self, part='delta'):
+        import tifffile
+        if part == 'delta':
+            tifffile.imshow(self.grid_delta)
+        elif part == 'beta':
+            tifffile.imshow(self.grid_beta)
+        else:
+            warnings.warn('Wrong part specified for show_grid.')
+
+    def initialize_wavefront(self, type, **kwargs):
+        """Initialize wavefront.
+
+        Parameters:
+        -----------
+        type : str
+            Type of wavefront to be initialized. Valid options:
+            'plane', 'spot', 'point_projection_lens', 'spherical'
+        kwargs :
+            Options specific to the selection of type.
+            'plane': no option
+            'spot': 'width'
+            'point_projection_lens': 'focal_length', 'lens_sample_dist'
+            'spherical': 'dist_to_source'
+        """
+        wave_shape = np.asarray(self.wavefront.shape)
+        if type == 'plane':
+            self.wavefront[...] = 1.
+        elif type == 'spot':
+            wid = kwargs['width']
+            radius = int(wid / 2)
+            if self._ndim == 2:
+                center = int(wave_shape[0] / 2)
+                self.wavefront[center-radius:center-radius+wid] = 1.
+            elif self._ndim == 3:
+                center = np.array(wave_shape / 2, dtype=int)
+                self.wavefront[center[0]-radius:center[0]-radius+wid, center[1]-radius:center[1]-radius+wid] = 1.
+        elif type == 'spherical':
+            z = kwargs['dist_to_source']
+            xx = self.mesh[0][:, :, 0]
+            yy = self.mesh[1][:, :, 0]
+            xx -= xx[0, -1] / 2
+            yy -= yy[-1, 0] / 2
+            print(xx, yy, z)
+            r = np.sqrt(xx ** 2 + yy ** 2 + z ** 2)
+            self.wavefront = np.exp(-1j * 2 * np.pi * r / self.lmbda_nm)
+        elif type == 'point_projection_lens':
+            f = kwargs['focal_length']
+            s = kwargs['lens_sample_dist']
+            xx = self.mesh[0][:, :, 0]
+            yy = self.mesh[1][:, :, 0]
+            xx -= xx[0, -1] / 2
+            yy -= yy[-1, 0] / 2
+            r = np.sqrt(xx ** 2 + yy ** 2)
+            theta = np.arctan(r / (s - f))
+            path = np.mod(s / np.cos(theta), self.lmbda_nm)
+            phase = path * 2 * PI
+            wavefront = np.ones(wave_shape).astype('complex64')
+            wavefront = wavefront + 1j * np.tan(phase)
+            self.wavefront = wavefront / np.abs(wavefront)
 
 
 def preprocess(dat, blur=None, normalize_bg=False):
@@ -117,9 +244,11 @@ def multislice_propagate(grid_delta, grid_beta, energy_ev, psize_cm):
     for i_slice in range(n_slice):
         print('Slice: {:d}'.format(i_slice))
         sys.stdout.flush()
-        delta_slice = grid_delta[:, :, i_slice].astype(np.complex64)
-        beta_slice = grid_beta[:, :, i_slice].astype(np.complex64)
-        c = tf.exp(1j * k * delta_slice * 1j) * tf.exp(-k * beta_slice)
+        delta_slice = grid_delta[:, :, i_slice]
+        delta_slice = tf.cast(delta_slice, dtype=tf.complex64)
+        beta_slice = grid_beta[:, :, i_slice]
+        beta_slice = tf.cast(beta_slice, dtype=tf.complex64)
+        c = tf.exp(1j * k * delta_slice) * tf.exp(-k * beta_slice)
         # c = tf.reshape(c, wavefront.shape)
         wavefront = wavefront * c
 
