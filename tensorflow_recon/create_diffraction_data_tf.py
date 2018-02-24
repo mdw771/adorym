b1b7ede4cc74d76d565704bead49f4bfa1d02460
mdw771 <mingdu2015@u.northwestern.edu>
2018-02-24 17:44:56 -0600

    changed stoc

--- tensorflow_recon/create_diffraction_data_tf.py
+++ tensorflow_recon/create_diffraction_data_tf.py
@@ -36,8 +36,8 @@ def rotate_and_project(i, obj):
 sess = tf.Session()
 
 # read model
-grid_delta = np.load('phantom/dual_sphere_delta.npy')
-grid_beta = np.load('phantom/dual_sphere_beta.npy')
+grid_delta = np.load('phantom/grid_delta.npy')
+grid_beta = np.load('phantom/grid_beta.npy')
 obj_init = np.zeros([img_dim, img_dim, img_dim, 2])
 obj_init[:, :, :, 0] = grid_delta
 obj_init[:, :, :, 1] = grid_beta
@@ -48,7 +48,7 @@ theta_ls = -np.linspace(theta_st, theta_end, n_theta)
 theta_ls_tensor = tf.constant(theta_ls, dtype='float32')
 
 # create data file
-f = h5py.File('data_diff_dual_sphere.h5', 'w')
+f = h5py.File('data_diff_tf_360.h5', 'w')
 grp = f.create_group('exchange')
 dat = grp.create_dataset('data', shape=(n_theta, grid_delta.shape[0], grid_delta.shape[1]), dtype=np.complex64)
 
