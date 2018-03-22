db9897a3e9d89dddfeb2e00618cb768fdeedabbe
mdw771 <mingdu2015@u.northwestern.edu>
2018-03-22 15:23:22 -0500

    md

--- tensorflow_recon/coarse_phase_retrival_and_recon.py
+++ tensorflow_recon/coarse_phase_retrival_and_recon.py
@@ -2,19 +2,21 @@ import h5py
 import numpy as np
 import tomopy
 import dxchange
-import time
+import time, os
+from scipy.ndimage import gaussian_filter
 
 
-f = h5py.File('data_diff_tf.h5', 'r')
+f = h5py.File('adhesin/data_adhesin_360_unity.h5', 'r')
 dat = f['exchange/data'][...]
 dat = np.abs(dat) ** 2
+dat = (dat - dat.min()) / (dat.max() - dat.min())
 
 dat = tomopy.retrieve_phase(dat,
-                            pixel_size=1e-7,
+                            pixel_size=0.67e-7,
                             dist=32e-7,
-                            alpha=1e-2,
+                            alpha=1e-3,
                             energy=5)
-dxchange.write_tiff(dat[0], 'paganin_obj/pr', dtype='float32', overwrite=True)
+dxchange.write_tiff(dat, 'adhesin/paganin_obj/pr/pr', dtype='float32', overwrite=True)
 dat = tomopy.minus_log(dat)
 
 
@@ -26,7 +28,14 @@ t0 = time.time()
 rec = tomopy.recon(dat,
                    theta=tomopy.angles(dat.shape[0]),
                    center=32,
-                   algorithm='sirt',
-                   **options)
+                   algorithm='gridrec',)
+                   # **options)
 print(time.time() - t0)
-dxchange.write_tiff_stack(rec, 'paganin_obj/recon', dtype='float32', overwrite=True)
+dxchange.write_tiff_stack(rec, 'adhesin/paganin_obj/recon', dtype='float32', overwrite=True)
+
+rec = gaussian_filter(np.abs(rec), sigma=1, mode='constant')
+mask = np.zeros_like(rec)
+# mask[rec > 3e-5] = 1
+mask[np.abs(rec) > 4e-4] = 1
+mask = tomopy.circ_mask(mask, 0, ratio=0.9)
+dxchange.write_tiff_stack(mask, os.path.join('adhesin', 'fin_sup_mask/mask'), dtype='float32', overwrite=True)
