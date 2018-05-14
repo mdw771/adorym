1dc5182eca5fd6b82871586883923661630eb583
Ming <mingdu2015@u.northwestern.edu>
2018-05-14 10:14:25 -0500

    core parallelization

--- tensorflow_recon/create_diffraction_data.py
+++ tensorflow_recon/create_diffraction_data.py
@@ -6,6 +6,8 @@ import h5py
 from scipy.ndimage.interpolation import rotate
 import numpy as np
 import dxchange
+import matplotlib.pyplot as plt
+import sys
 
 
 # ============================================
@@ -17,24 +19,24 @@ PI = 3.1415927
 
 # ============================================
 theta_st = 0
-theta_end = PI
+theta_end = 2 * PI
 n_theta = 500
-img_dim = 64
+img_dim = 256
 alpha = 1.e-4
 # ============================================
 
 
 # read model
-grid_delta = np.load('em_data/adhesin_delta.npy')
-grid_beta = np.load('em_data/adhesin_beta.npy')
+grid_delta = np.load('cone_256_filled/phantom/grid_delta.npy')
+grid_beta = np.load('cone_256_filled/phantom/grid_beta.npy')
 
 # list of angles
 theta_ls = -np.linspace(theta_st, theta_end, n_theta)
 
 # create data file
-f = h5py.File('data_adhesin_360_soft.h5', 'w')
-grp = f.create_group('exchange')
-dat = grp.create_dataset('data', shape=(n_theta, grid_delta.shape[0], grid_delta.shape[1]), dtype=np.complex64)
+# f = h5py.File('test', 'w')
+# grp = f.create_group('exchange')
+# dat = grp.create_dataset('data', shape=(n_theta, grid_delta.shape[0], grid_delta.shape[1]), dtype=np.complex64)
 
 for i, theta in enumerate(theta_ls):
 
@@ -42,14 +44,19 @@ for i, theta in enumerate(theta_ls):
     this_delta = rotate(grid_delta, np.rad2deg(theta), axes=(1, 2), reshape=False)
     this_beta = rotate(grid_beta, np.rad2deg(theta), axes=(1, 2), reshape=False)
 
-    sim = Simulator(energy=1000,
+    sim = Simulator(energy=5000,
                     grid=(this_delta, this_beta),
                     psize=[1e-7, 1e-7, 1e-7])
 
     sim.initialize_wavefront('plane')
-    wavefront = sim.multislice_propagate(free_prop_dist=None)
+    wavefront = sim.multislice_propagate(free_prop_dist=1e-4)
+    wavefront = abs(wavefront)
+    plt.imshow(wavefront)
+    plt.show()
+    sys.exit()
 
-    dat[i, :, :] = wavefront
-    dxchange.write_tiff(np.abs(wavefront), 'diffraction_dat_tf/mag_{:05d}'.format(i), overwrite=True, dtype='float32')
 
-f.close()
+    # dat[i, :, :] = wavefront
+    # dxchange.write_tiff(np.abs(wavefront), 'diffraction_dat_tf/mag_{:05d}'.format(i), overwrite=True, dtype='float32')
+#
+# f.close()
