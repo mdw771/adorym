dd08321ac46a6896fdfedb87a02351e3f718f694
mdw771 <mingdu2015@u.northwestern.edu>
2018-05-22 14:38:55 -0500

    allow initial guess

--- tensorflow_recon/diffraction_tomo_tf_stochastic.py
+++ tensorflow_recon/diffraction_tomo_tf_stochastic.py
@@ -12,9 +12,10 @@ PI = 3.1415927
 theta_st = 0
 theta_end = 2 * PI
 n_epochs = 'auto'
+n_epoch_final_pass = 5
 alpha_d_ls = [1.5e-6]
 alpha_b_ls = [alpha_d_ls[0] * 0.1]
-gamma_ls = [1e-6]
+gamma_ls = [5e-7]
 learning_rate_ls = [1e-7]
 center = 128
 energy_ev = 5000
@@ -24,6 +25,9 @@ n_epochs_mask_release = 10
 free_prop_cm = 1e-4
 shrink_cycle = 1
 multiscale_level = 3
+# initial_guess = ['cone_256_filled/recon_360_minibatch_10_mskrls_10_shrink_1_iter_auto_alphad_1.5e-06_alphab_1.5000000000000002e-07_gamma_1e-07_rate_1e-07_energy_5000_size_256_ntheta_500_prop_0.0001_ms_3_cpu_True/delta_ds_1.tiff',
+#                  'cone_256_filled/recon_360_minibatch_10_mskrls_10_shrink_1_iter_auto_alphad_1.5e-06_alphab_1.5000000000000002e-07_gamma_1e-07_rate_1e-07_energy_5000_size_256_ntheta_500_prop_0.0001_ms_3_cpu_True/beta_ds_1.tiff']
+initial_guess = None
 # ============================================
 
 
@@ -53,4 +57,6 @@ if __name__ == '__main__':
                                  cpu_only=True,
                                  free_prop_cm=free_prop_cm,
                                  shrink_cycle=shrink_cycle,
-                                 multiscale_level=multiscale_level)
+                                 multiscale_level=multiscale_level,
+                                 n_epoch_final_pass=n_epoch_final_pass,
+                                 initial_guess=initial_guess)
