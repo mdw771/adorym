0c268a07c388bf97c3d5c6cc12039437b2d33989
Ming <mingdu2015@u.northwestern.edu>
2018-05-29 10:12:36 -0500

    clean repo

--- tensorflow_recon/cone_256_filled/generate_xray_figures.py
+++
@@ -1,9 +0,0 @@
-import dxchange
-import numpy as np
-from scipy.misc import imsave
-
-# a = dxchange.read_tiff('delta_ds_1.tiff')
-a = dxchange.read_tiff_stack('recon/recon_00000.tiff', range(256), 5)
-dxchange.write_tiff(np.sum(a, axis=0), 'cone_xray_top_conv')
-dxchange.write_tiff(np.sum(a, axis=2), 'cone_xray_left_conv')
-dxchange.write_tiff(np.sum(a, axis=1), 'cone_xray_front_conv')
