ca2257afaf223db9c254d0032b3bc0448cee8a71
mdw771 <mingdu2015@u.northwestern.edu>
2018-08-08 16:40:39 -0500

    sim generator

--- tensorflow_recon/create_ptycho_data.py
+++ tensorflow_recon/create_ptycho_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from simulation import create_ptychography_data
+from simulation import create_ptychography_data_batch_numpy
 
 
 # ============================================
@@ -45,6 +45,6 @@ probe_phase_max = 0.5
 probe_pos = [(y, x) for y in np.linspace(18, 120, 35) for x in np.linspace(54, 198, 49)] + \
             [(y, x) for y in np.linspace(120, 222, 35) for x in np.linspace(22, 230, 70)]
 
-create_ptychography_data(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
-                         probe_type='gaussian', probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
-                         probe_mag_sigma=probe_mag_sigma, probe_phase_sigma=probe_phase_sigma, probe_phase_max=probe_phase_max, probe_circ_mask=None)
+create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
+                                     probe_type='gaussian', probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
+                                     probe_mag_sigma=probe_mag_sigma, probe_phase_sigma=probe_phase_sigma, probe_phase_max=probe_phase_max, probe_circ_mask=None)
