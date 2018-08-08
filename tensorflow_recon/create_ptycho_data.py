89d002c5a39c7f6dd72bcaa85c7701d121b6ba6f
mdw771 <mingdu2015@u.northwestern.edu>
2018-08-08 16:36:20 -0500

    sim generator

--- tensorflow_recon/create_ptycho_data.py
+++ tensorflow_recon/create_ptycho_data.py
@@ -18,19 +18,18 @@ energy_ev = 5000
 # energy_ev = 800
 psize_cm = 1.e-7
 # psize_cm = 0.67e-7
-free_prop_cm = 1.e-4
 # free_prop_cm = None
 phantom_path = 'cone_256_filled_ptycho/phantom'
 # phantom_path = 'adhesin_ptycho/phantom'
 save_folder = 'cone_256_filled_ptycho'
 # save_folder = 'adhesin_ptycho'
 # fname = 'data_adhesin_64_1nm_1um.h5'
-fname = 'data_cone_64_1nm_1um.h5'
-probe_size = [72, 72]
+fname = 'data_cone_256_1nm_1um.h5'
+probe_size = [18, 18]
 # probe_size = [18, 18]
-probe_mag_sigma = 40
+probe_mag_sigma = 10
 # probe_mag_sigma = 2
-probe_phase_sigma = 40
+probe_phase_sigma = 10
 # probe_phase_sigma = 2
 probe_phase_max = 0.5
 # ============================================
@@ -41,8 +40,10 @@ probe_phase_max = 0.5
 # probe_phase = gaussian_filter(probe_phase, 3)
 # wavefront_initial = [probe_mag, probe_phase]
 
-probe_pos = [(y, x) for y in np.linspace(36, 220, 23) for x in np.linspace(36, 220, 23)]
+# probe_pos = [(y, x) for y in np.linspace(36, 220, 23) for x in np.linspace(36, 220, 23)]
 # probe_pos = [(y, x) for y in np.linspace(9, 55, 23) for x in np.linspace(9, 55, 23)]
+probe_pos = [(y, x) for y in np.linspace(18, 120, 35) for x in np.linspace(54, 198, 49)] + \
+            [(y, x) for y in np.linspace(120, 222, 35) for x in np.linspace(22, 230, 70)]
 
 create_ptychography_data(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
                          probe_type='gaussian', probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
