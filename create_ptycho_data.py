fix fresnel kernel sign

create_ptycho_data.py
@@ -14,16 +14,18 @@ PI = 3.1415927
 theta_st = 0
 theta_end = 2 * PI
 n_theta = 500
-energy_ev = 5000
-# energy_ev = 800
-psize_cm = 1.e-7
-# psize_cm = 0.67e-7
-# free_prop_cm = None
+# energy_ev = 5000
+energy_ev = 800
+# psize_cm = 1.e-7
+psize_cm = 0.67e-7
+free_prop_cm = None
 # phantom_path = 'cell/ptychography/phantom'
-phantom_path = 'cone_256_foam_ptycho/phantom'
+# phantom_path = 'cone_256_foam_ptycho/phantom'
+phantom_path = 'adhesin_ptycho_2/phantom'
 # save_folder = 'cell/ptychography'
-save_folder = 'cone_256_foam_ptycho'
-fname = 'data_cone_256_foam_1nm.h5'
+save_folder = 'adhesin_ptycho_2'
+# fname = 'data_cone_256_foam_1nm.h5'
+fname = 'data_adhesin_64_1nm_1um.h5'
 # fname = 'data_cell_phase.h5'
 probe_size = [72, 72]
 # probe_size = [18, 18]
@@ -40,13 +42,13 @@ probe_phase_max = 0.5
 # probe_phase = gaussian_filter(probe_phase, 3)
 # wavefront_initial = [probe_mag, probe_phase]
 
-probe_pos = [(y, x) for y in np.arange(23) * 12 for x in np.arange(23) * 12 ]
+# probe_pos = [(y, x) for y in np.arange(23) * 12 for x in np.arange(23) * 12 ]
 # probe_pos = [(y, x) for y in np.linspace(340, 665, 27) for x in np.linspace(340, 665, 27)]
 # probe_pos = [(y, x) for y in np.arange(33) * 10 for x in np.arange(34) * 10]
-# probe_pos = [(y, x) for y in np.linspace(9, 55, 23) for x in np.linspace(9, 55, 23)]
+probe_pos = [(int(y), int(x)) for y in np.linspace(9, 55, 23) for x in np.linspace(9, 55, 23)]
 # probe_pos = [(y, x) for y in np.linspace(18, 120, 35) for x in np.linspace(54, 198, 49)] + \
 #             [(y, x) for y in np.linspace(120, 222, 35) for x in np.linspace(22, 230, 70)]
 
-create_ptychography_data(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
-                         probe_type='gaussian', probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
-                         probe_mag_sigma=probe_mag_sigma, probe_phase_sigma=probe_phase_sigma, probe_phase_max=probe_phase_max, probe_circ_mask=None)
+create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
+                                     probe_type='gaussian', probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
+                                     probe_mag_sigma=probe_mag_sigma, probe_phase_sigma=probe_phase_sigma, probe_phase_max=probe_phase_max, probe_circ_mask=None)

ptychography.py
@@ -15,23 +15,26 @@ plt.switch_backend('agg')
 PI = 3.1415927
 
 
-def reconstruct_ptychography(fname, probe_pos, probe_size, obj_size, theta_st=0, theta_end=PI, theta_downsample=None,
+def reconstruct_ptychography(fname, probe_pos, probe_size, obj_size, theta_st=0, theta_end=PI, n_theta=None, theta_downsample=None,
                              n_epochs='auto', crit_conv_rate=0.03, max_nepochs=200,
                              alpha=1e-7, alpha_d=None, alpha_b=None, gamma=1e-6, learning_rate=1.0,
                              output_folder=None, minibatch_size=None, save_intermediate=False, full_intermediate=False,
                              energy_ev=5000, psize_cm=1e-7, cpu_only=False, save_path='.',
-                             phantom_path='phantom', core_parallelization=True, free_prop_cm=None,
+                             core_parallelization=True, free_prop_cm=None,
                              multiscale_level=1, n_epoch_final_pass=None, initial_guess=None, n_batch_per_update=1,
                              dynamic_rate=True, probe_type='gaussian', probe_initial=None, probe_learning_rate=1e-3,
                              pupil_function=None, probe_circ_mask=0.9, finite_support_mask=None,
                              forward_algorithm='fresnel', dynamic_dropping=False, dropping_threshold=8e-5,
-                             n_dp_batch=20, object_type='normal', fresnel_approx=False, **kwargs):
+                             n_dp_batch=20, object_type='normal', fresnel_approx=False, pure_projection=False, two_d_mode=False, **kwargs):
 
     def calculate_loss(obj_delta, obj_beta, probe_real, probe_imag, this_i_theta, this_pos_batch, this_prj_batch):
 
         obj_stack = np.stack([obj_delta, obj_beta], axis=3)
-        obj_rot = apply_rotation(obj_stack, coord_ls[this_i_theta],
-                                 'arrsize_{}_{}_{}_ntheta_{}'.format(*this_obj_size, n_theta))
+        if not two_d_mode:
+            obj_rot = apply_rotation(obj_stack, coord_ls[this_i_theta],
+                                     'arrsize_{}_{}_{}_ntheta_{}'.format(*this_obj_size, n_theta))
+        else:
+            obj_rot = obj_stack
         probe_pos_batch_ls = []
         exiting_ls = []
         i_dp = 0
@@ -57,7 +60,8 @@ def reconstruct_ptychography(fname, probe_pos, probe_size, obj_size, theta_st=0,
             subobj_ls = np.stack(subobj_ls)
             exiting = multislice_propagate_batch_numpy(subobj_ls[:, :, :, :, 0], subobj_ls[:, :, :, :, 1], probe_real,
                                                        probe_imag, energy_ev, psize_cm * ds_level, kernel=h, free_prop_cm='inf',
-                                                       obj_batch_shape=[len(pos_batch), *probe_size, this_obj_size[-1]])
+                                                       obj_batch_shape=[len(pos_batch), *probe_size, this_obj_size[-1]],
+                                                       fresnel_approx=fresnel_approx, pure_projection=pure_projection)
             exiting_ls.append(exiting)
         exiting_ls = np.concatenate(exiting_ls, 0)
         loss = np.mean((np.abs(exiting_ls) - np.abs(this_prj_batch)) ** 2)
@@ -75,7 +79,10 @@ def reconstruct_ptychography(fname, probe_pos, probe_size, obj_size, theta_st=0,
     print_flush('Reading data...', designate_rank=0, this_rank=rank)
     f = h5py.File(os.path.join(save_path, fname), 'r')
     prj = f['exchange/data']
-    n_theta = prj.shape[0]
+    if n_theta is None:
+        n_theta = prj.shape[0]
+    if two_d_mode:
+        n_theta = 1
     prj_theta_ind = np.arange(n_theta, dtype=int)
     theta = -np.linspace(theta_st, theta_end, n_theta, dtype='float32')
     if theta_downsample is not None:
@@ -252,9 +259,16 @@ def reconstruct_ptychography(fname, probe_pos, probe_size, obj_size, theta_st=0,
 
             t00 = time.time()
             print_flush('Allocating jobs over threads...', designate_rank=0, this_rank=rank)
-            # Make a list of all thetas and spot positions
-            theta_ls = np.arange(n_theta)
-            np.random.shuffle(theta_ls)
+            # Make a list of all thetas and spot positions'
+            if not two_d_mode:
+                theta_ls = np.arange(n_theta)
+                np.random.shuffle(theta_ls)
+            else:
+                theta_ls = np.linspace(0, 2 * PI, prj.shape[0])
+                theta_ls = abs(theta_ls - theta_st) < 1e-5
+                i_theta = np.nonzero(theta_ls)[0][0]
+                theta_ls = np.array([i_theta])
+                print(theta_ls)
             for i, i_theta in enumerate(theta_ls):
                 spots_ls = range(n_pos)
                 if n_pos % minibatch_size != 0:
@@ -373,12 +387,12 @@ def reconstruct_ptychography_hdf5(fname, probe_pos, probe_size, obj_size, theta_
                                   alpha=1e-7, alpha_d=None, alpha_b=None, gamma=1e-6, learning_rate=1.0,
                                   output_folder=None, minibatch_size=None, save_intermediate=False, full_intermediate=False,
                                   energy_ev=5000, psize_cm=1e-7, cpu_only=False, save_path='.',
-                                  phantom_path='phantom', core_parallelization=True, free_prop_cm=None,
+                                  core_parallelization=True, free_prop_cm=None,
                                   multiscale_level=1, n_epoch_final_pass=None, initial_guess=None, n_batch_per_update=1,
                                   dynamic_rate=True, probe_type='gaussian', probe_initial=None, probe_learning_rate=1e-3, probe_learning_rate_init=1e-4,
                                   pupil_function=None, probe_circ_mask=0.9, finite_support_mask=None,
                                   forward_algorithm='fresnel', dynamic_dropping=True, dropping_threshold=8e-5,
-                                  n_dp_batch=20, object_type='normal', fresnel_approx=False, **kwargs):
+                                  n_dp_batch=20, object_type='normal', fresnel_approx=False, pure_projection=False, **kwargs):
 
     def calculate_loss(obj_delta, obj_beta, probe_real, probe_imag, this_pos_batch, this_prj_batch):
 
@@ -395,7 +409,8 @@ def reconstruct_ptychography_hdf5(fname, probe_pos, probe_size, obj_size, theta_
             subobj_ls_beta = obj_beta[pos_ind:pos_ind + len(pos_batch), :, :, :]
             exiting = multislice_propagate_batch_numpy(subobj_ls_delta, subobj_ls_beta, probe_real,
                                                        probe_imag, energy_ev, psize_cm * ds_level, kernel=h, free_prop_cm='inf',
-                                                       obj_batch_shape=[len(pos_batch), *probe_size, this_obj_size[-1]])
+                                                       obj_batch_shape=[len(pos_batch), *probe_size, this_obj_size[-1]],
+                                                       fresnel_approx=fresnel_approx, pure_projection=pure_projection)
             exiting_ls.append(exiting)
             pos_ind += len(pos_batch)
         exiting_ls = np.concatenate(exiting_ls, 0)
@@ -554,6 +569,7 @@ def reconstruct_ptychography_hdf5(fname, probe_pos, probe_size, obj_size, theta_
                 probe_mag, probe_phase = probe_initial
                 probe_real, probe_imag = mag_phase_to_real_imag(probe_mag, probe_phase)
             else:
+                print_flush('Estimating probe from measured data...', 0, rank)
                 probe_init = create_probe_initial_guess_ptycho(os.path.join(save_path, fname))
                 probe_real = probe_init.real
                 probe_imag = probe_init.imag
@@ -731,3 +747,5 @@ def reconstruct_ptychography_hdf5(fname, probe_pos, probe_size, obj_size, theta_
                                 overwrite=True)
             print_flush('Current iteration finished.', designate_rank=0, this_rank=rank)
         comm.Barrier()
+
+

reconstruct_ptycho_init.py
@@ -1,32 +1,73 @@
 from ptychography import reconstruct_ptychography, reconstruct_ptychography_hdf5
 import numpy as np
 import dxchange
+import h5py
+import os
 
 init_delta = dxchange.read_tiff('cone_256_filled_ptycho/n2e5/xrmlite_iter2/delta_ds_1.tiff')
 init_beta = dxchange.read_tiff('cone_256_filled_ptycho/n2e5/xrmlite_iter2/beta_ds_1.tiff')
 init = [init_delta, init_beta]
 
-params_adhesin = {'fname': 'data_adhesin_64_1nm_1um.h5',
+# params_adhesin = {'fname': 'data_adhesin_64_1nm_1um.h5',
+#                   'theta_st': 0,
+#                   'theta_end': 2 * np.pi,
+#                   'theta_downsample': None,
+#                   'n_epochs': 10,
+#                   'obj_size': (64, 64, 64),
+#                   'alpha_d': 0,
+#                   'alpha_b': 0,
+#                   'gamma': 0,
+#                   'probe_size': (18, 18),
+#                   # 'learning_rate': 1e-7,
+#                   'learning_rate': 1e-9,
+#                   'center': 32,
+#                   'energy_ev': 800,
+#                   'psize_cm': 0.67e-7,
+#                   'minibatch_size': 23,
+#                   'n_batch_per_update': 1,
+#                   'output_folder': 'test',
+#                   'cpu_only': True,
+#                   'save_path': 'adhesin_ptycho',
+#                   'multiscale_level': 1,
+#                   'n_epoch_final_pass': None,
+#                   'save_intermediate': True,
+#                   'full_intermediate': True,
+#                   'initial_guess': None,
+#                   'n_dp_batch': 529,
+#                   'fresnel_approx': True,
+#                   'probe_type': 'gaussian',
+#                   'probe_learning_rate': 1e-3,
+#                   'probe_learning_rate_init': 1e-2,
+#                   'finite_support_mask': None,
+#                   'forward_algorithm': 'fresnel',
+#                   'object_type': 'normal',
+#                   'probe_pos': [(y, x) for y in np.linspace(9, 55, 23, dtype=int) for x in np.linspace(9, 55, 23, dtype=int)],
+#                   'probe_mag_sigma': 10,
+#                   'probe_phase_sigma': 10,
+#                   'probe_phase_max': 0.5,
+#                   }
+
+params_adhesin_2 = {'fname': 'data_adhesin_64_1nm_1um.h5',
                   'theta_st': 0,
-                  'theta_end': 2 * np.pi,
+                  'theta_end': 0,
+                  'n_theta': 1,
                   'theta_downsample': None,
-                  'n_epochs': 10,
-                  'obj_size': (64, 64, 64),
+                  'n_epochs': 500,
+                  'obj_size': (64, 64, 1),
                   'alpha_d': 0,
                   'alpha_b': 0,
                   'gamma': 0,
-                  'probe_size': (18, 18),
+                  'probe_size': (72, 72),
                   # 'learning_rate': 1e-7,
                   'learning_rate': 1e-9,
                   'center': 32,
                   'energy_ev': 800,
                   'psize_cm': 0.67e-7,
-                  'minibatch_size': 23,
+                  'minibatch_size': 23*23,
                   'n_batch_per_update': 1,
-                  'output_folder': 'test',
+                  'output_folder': 'test_9_init',
                   'cpu_only': True,
-                  'save_path': 'adhesin_ptycho',
-                  'phantom_path': 'adhesin_ptycho/phantom',
+                  'save_path': 'adhesin_ptycho_2',
                   'multiscale_level': 1,
                   'n_epoch_final_pass': None,
                   'save_intermediate': True,
@@ -34,15 +75,15 @@ params_adhesin = {'fname': 'data_adhesin_64_1nm_1um.h5',
                   'initial_guess': None,
                   'n_dp_batch': 529,
                   'fresnel_approx': True,
-                  'probe_type': 'gaussian',
+                  'probe_type': 'optimizable',
                   'probe_learning_rate': 1e-3,
-                  'probe_learning_rate_init': 1e-2,
+                  'probe_learning_rate_init': 1e-3,
                   'finite_support_mask': None,
                   'forward_algorithm': 'fresnel',
                   'object_type': 'normal',
                   'probe_pos': [(y, x) for y in np.linspace(9, 55, 23, dtype=int) for x in np.linspace(9, 55, 23, dtype=int)],
-                  'probe_mag_sigma': 10,
-                  'probe_phase_sigma': 10,
+                  'probe_mag_sigma': 6,
+                  'probe_phase_sigma': 6,
                   'probe_phase_max': 0.5,
                   }
 
@@ -65,7 +106,6 @@ params_cone_marc = {'fname': 'data_cone_256_1nm_marc.h5',
                     'output_folder': 'test',
                     'cpu_only': True,
                     'save_path': 'cone_256_filled_ptycho',
-                    'phantom_path': 'cone_256_filled_ptycho/phantom',
                     'multiscale_level': 1,
                     'n_epoch_final_pass': None,
                     'save_intermediate': True,
@@ -226,8 +266,8 @@ params_cone = {'fname': 'data_cone_256_1nm_marc.h5',
                'finite_support_mask': dxchange.read_tiff('cone_256_filled_ptycho/mask.tiff')
                }
 
-params = params_2d_cell
 # params = params_2d_cell
+params = params_adhesin_2
 
 
 # reconstruct_ptychography(fname=params['fname'],
@@ -311,4 +351,30 @@ params = params_2d_cell
 #                               fresnel_approx=params['fresnel_approx'],
 #                               **params['probe_options'])
 
-reconstruct_ptychography(**params)
+# reconstruct_ptychography(**params)
+
+f = h5py.File(os.path.join(params['save_path'], params['fname']))
+n_theta_original = f['exchange/data'].shape[0]
+i_theta_ls = np.arange(n_theta_original)
+theta_ls = np.linspace(0, 2 * np.pi, n_theta_original)
+
+theta_dual_ls = np.stack([i_theta_ls, theta_ls]).transpose()
+
+for i, (i_theta, theta) in enumerate(theta_dual_ls[::10]):
+
+    i_theta = int(i_theta)
+    print('Now reconstruction theta ID: {}'.format(i_theta))
+    params['two_d_mode'] = True
+    params['n_theta'] = 1
+    params['theta_st'] = theta
+    if i_theta == 0:
+        params['probe_initial'] = None
+    else:
+        params['probe_initial'] = [dxchange.read_tiff(os.path.join(params['save_path'], params['output_folder'], 'probe_mag_ds_1.tiff')),
+                                   dxchange.read_tiff(os.path.join(params['save_path'], params['output_folder'], 'probe_phase_ds_1.tiff'))]
+    reconstruct_ptychography(**params)
+    os.rename(os.path.join(params['save_path'], params['output_folder'], 'delta_ds_1.tiff'),
+              os.path.join(params['save_path'], params['output_folder'], 'delta_itheta_{}.tiff'.format(i_theta)))
+    os.rename(os.path.join(params['save_path'], params['output_folder'], 'beta_ds_1.tiff'),
+              os.path.join(params['save_path'], params['output_folder'], 'beta_itheta_{}.tiff'.format(i_theta)))
+

util.py
@@ -153,7 +153,7 @@ def gen_mesh(max, shape):
     return res
 
 
-def get_kernel(dist_nm, lmbda_nm, voxel_nm, grid_shape, fresnel_approx=False):
+def get_kernel(dist_nm, lmbda_nm, voxel_nm, grid_shape, fresnel_approx=True):
     """Get Fresnel propagation kernel for TF algorithm.
 
     Parameters:
@@ -169,7 +169,7 @@ def get_kernel(dist_nm, lmbda_nm, voxel_nm, grid_shape, fresnel_approx=False):
     u, v = gen_mesh([v_max, u_max], grid_shape[0:2])
     # H = np.exp(1j * k * dist_nm * np.sqrt(1 - lmbda_nm**2 * (u**2 + v**2)))
     if fresnel_approx:
-        H = np.exp(-1j * PI * lmbda_nm * dist_nm * (u**2 + v**2)) # NOTE: may need to inverse sign
+        H = np.exp(1j * PI * lmbda_nm * dist_nm * (u**2 + v**2))
     else:
         quad = 1 - lmbda_nm ** 2 * (u**2 + v**2)
         quad_inner = np.clip(quad, a_min=0, a_max=None)
@@ -260,7 +260,9 @@ def exp_j(a):
     return np.cos(a) + 1j * np.sin(a)
 
 
-def multislice_propagate_batch_numpy(grid_delta_batch, grid_beta_batch, probe_real, probe_imag, energy_ev, psize_cm, free_prop_cm=None, obj_batch_shape=None, kernel=None, fresnel_approx=False):
+def multislice_propagate_batch_numpy(grid_delta_batch, grid_beta_batch, probe_real, probe_imag, energy_ev, psize_cm,
+                                     free_prop_cm=None, obj_batch_shape=None, kernel=None, fresnel_approx=True,
+                                     pure_projection=False):
 
     minibatch_size = obj_batch_shape[0]
     grid_shape = obj_batch_shape[1:]
@@ -286,7 +288,7 @@ def multislice_propagate_batch_numpy(grid_delta_batch, grid_beta_batch, probe_re
         beta_slice = grid_beta_batch[:, :, :, i]
         c = exp_j(k * delta_slice) * np.exp(-k * beta_slice)
         wavefront = wavefront * c
-        if i < n_slice - 1:
+        if i < n_slice - 1 and not pure_projection:
             wavefront = np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(wavefront), axes=[1, 2]) * h, axes=[1, 2]))
 
     if free_prop_cm is not None: