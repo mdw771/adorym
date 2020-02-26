a8cd515924dc361797dd32e53293e12cd2b9040a
mdw771 <mingdu2015@u.northwestern.edu>
2020-02-26 11:06:08 -0600

    simulation docs

--- create_ptycho_data.py
+++ create_ptycho_data.py
@@ -1,62 +1,102 @@
-import numpy as np
-
 from simulation import *
-
-
-# ============================================
-# DO NOT ROTATE PROGRESSIVELY
-# (DO NOT CONTINUE TO ROTATE AN INTERPOLATED OBJECT)
-# ============================================
 
 PI = 3.1415927
 
-# ============================================
-theta_st = 0
-theta_end = 2 * PI
-n_theta = 500
-energy_ev = 5000
-# energy_ev = 800
-psize_cm = 1.e-7
-# psize_cm = 0.67e-7
-free_prop_cm = 'inf'
-# phantom_path = 'cell/ptychography/phantom'
-phantom_path = 'cone_256_foam_ptycho/phantom'
-# phantom_path = 'adhesin_ptycho_2/phantom'
-# save_folder = 'cell/ptychography'
-save_folder = 'cone_256_foam_ptycho'
-# save_folder = 'adhesin_ptycho_2'
-fname = 'data_cone_256_foam_1nm_2.h5'
-# fname = 'data_adhesin_64_1nm_1um_.h5'
-# fname = 'data_cell_phase.h5'
-probe_size = [72, 72]
-# probe_size = [18, 18]
-probe_type = 'gaussian'
-probe_mag_sigma = 6
-# probe_mag_sigma = 2
-probe_phase_sigma = 6
-# probe_phase_sigma = 2
-probe_phase_max = 0.5
-# ============================================
+#==============================================================#
+#             Guidelines for parameter settings                #
+#==============================================================#
+# 1. Keep 'free_prop_cm = 'inf' for far-field imaging.         #
+# 2. Ptychography scan positions are now interpreted as the    #
+#    top-left corner of the probe array in object frame, NOT   #
+#    center.                                                   #
+# 3. Generate fullfield/single-shot images by giving only one  #
+#    pair of coordinates in probe_pos. There is no longer      #
+#    dedicated script/function for fullfield simulation.       #
+# 4. Output file will be saved as a HDF5 at save_path/fname.   #
+# 5. probe_type can be 'gaussian', 'plane', or 'fixed'. If set #
+#    to 'fixed', you must supply 2D wavefield values to        #
+#    probe_init as [probe_real, probe_imag]. If set to         #
+#    'gaussian', you should specify 'probe_mag_sigma',         #
+#    'probe_phase_sigma', and 'probe_phase_max'.               #
+# 6. Delta and beta of the refractive index of the simulated   #
+#    object are read from Numpy data files named as            #
+#    'grid_delta.npy' and 'grid_beta.npy' contained in the     #
+#    path given in 'phantom_path'.                             #
+#==============================================================#
 
-# probe_mag = np.ones([img_dim, img_dim], dtype=np.float32)
-# probe_phase = np.zeros([img_dim, img_dim], dtype=np.float32)
-# probe_phase[int(img_dim / 2), int(img_dim / 2)] = 0.1
-# probe_phase = gaussian_filter(probe_phase, 3)
-# wavefront_initial = [probe_mag, probe_phase]
+params_adhesin_2 = {'fname': 'data_adhesin_64_1nm_1um_2.h5',
+                  'theta_st': 0,
+                  'theta_end': 2 * np.pi,
+                  'n_theta': 500,
+                  'theta_downsample': None,
+                  'obj_size': (64, 64, 64),
+                  'probe_size': (72, 72),
+                  'energy_ev': 800,
+                  'psize_cm': 0.67e-7,
+                  'phantom_path': 'adhesin/phantom',
+                  'minibatch_size': 23,
+                  'cpu_only': True,
+                  'save_path': 'adhesin_ptycho_2',
+                  'probe_initial': None,
+                  'fresnel_approx': True,
+                  'probe_type': 'gaussian',
+                  'forward_algorithm': 'fresnel',
+                  'object_type': 'normal',
+                  'probe_pos': [(y, x) for y in np.linspace(-27, 19, 23, dtype=int) for x in np.linspace(-27, 19, 23, dtype=int)],
+                  'probe_mag_sigma': 6,
+                  'probe_phase_sigma': 6,
+                  'probe_phase_max': 0.5,
+                  'free_prop_cm': 'inf'
+                  }
 
-probe_pos = [(y, x) for y in np.arange(23) * 12 for x in np.arange(23) * 12 ]
-# probe_pos = [(y, x) for y in np.linspace(340, 665, 27) for x in np.linspace(340, 665, 27)]
-# probe_pos = [(y, x) for y in np.arange(33) * 10 for x in np.arange(34) * 10]
-# probe_pos = [(int(y), int(x)) for y in np.linspace(9, 55, 23) for x in np.linspace(9, 55, 23)]
-# probe_pos = [(y, x) for y in np.linspace(18, 120, 35) for x in np.linspace(54, 198, 49)] + \
-#             [(y, x) for y in np.linspace(120, 222, 35) for x in np.linspace(22, 230, 70)]
+params_cone_marc = {'fname': 'data_cone_256_foam_1nm.h5',
+                    'theta_st': 0,
+                    'theta_end': 2 * np.pi,
+                    'n_theta': 500,
+                    'theta_downsample': None,
+                    'obj_size': (256, 256, 256),
+                    'probe_size': (72, 72),
+                    'energy_ev': 5000,
+                    'psize_cm': 1.e-7,
+                    'phantom_path': 'cone_256_foam_ptycho/phantom',
+                    'minibatch_size': 1,
+                    'save_path': 'cone_256_foam_ptycho',
+                    'initial_guess': None,
+                    'probe_type': 'gaussian',
+                    'forward_algorithm': 'fresnel',
+                    'probe_pos': [(y, x) for y in (np.arange(23) * 12) - 36 for x in (np.arange(23) * 12) - 36],
+                    'probe_mag_sigma': 6,
+                    'probe_phase_sigma': 6,
+                    'probe_phase_max': 0.5,
+                    'free_prop_cm': 'inf'
+                    }
 
-###############################################################
-# Newer versions from now on will interpret probe_pos as top-left corners,
-# instead of centers, of probe arrays in the object frame.
-probe_pos = np.array(probe_pos) - np.array(probe_size) // 2
-###############################################################
 
-create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
-                                     probe_type=probe_type, free_prop_cm=free_prop_cm, probe_size=probe_size, theta_st=theta_st, theta_end=theta_end,
-                                     probe_mag_sigma=probe_mag_sigma, probe_phase_sigma=probe_phase_sigma, probe_phase_max=probe_phase_max, probe_circ_mask=None, fresnel_approx=True)
+params_2d_cell = {'fname': 'data_cell_phase_n1e9_ref.h5',
+                    'theta_st': 0,
+                    'theta_end': 0,
+                    'n_theta': 1,
+                    'theta_downsample': 1,
+                    'obj_size': (325, 325, 1),
+                    'probe_size': (72, 72),
+                    'energy_ev': 5000,
+                    'psize_cm': 1.e-7,
+                    'phantom_path': 'cell/phantom',
+                    'minibatch_size': 1,
+                    'n_batch_per_update': 1,
+                    'output_folder': 'n1e9_ref',
+                    'cpu_only': True,
+                    'save_path': 'cell/ptychography',
+                    'probe_type': 'gaussian',
+                    'probe_mag_sigma': 6,
+                    'probe_phase_sigma': 6,
+                    'probe_phase_max': 0.5,
+                    'forward_algorithm': 'fresnel',
+                    'object_type': 'phase_only',
+                    'probe_pos': [(y, x) for y in (np.arange(33) * 10) - 36 for x in (np.arange(34) * 10) - 36],
+                    'free_prop_cm': 'inf'
+                    }
+
+params = params_adhesin_2
+
+create_ptychography_data_batch_numpy(**params)
--- simulation.py
+++ simulation.py
@@ -14,94 +14,9 @@ from util import *
 from propagate import *
 
 
-def create_fullfield_data_numpy(energy_ev, psize_cm, free_prop_cm, n_theta, phantom_path, save_folder, fname, batch_size=1,
-                                probe_type='plane', wavefront_initial=None, theta_st=0, theta_end=2*PI, monitor_output=False, **kwargs):
-
-    def rotate_and_project(this_theta_batch, obj):
-        obj_rot_batch = []
-        for theta in this_theta_batch:
-            obj_rot_batch.append(sp_rotate(obj, theta, reshape=False, axes=(1, 2)))
-        obj_rot_batch = np.array(obj_rot_batch)
-        if probe_type == 'point':
-            raise DeprecationWarning
-            exiting = multislice_propagate_spherical_numpy(obj_rot_batch[:, :, :, :, 0], obj_rot_batch[:, :, :, :, 1],
-                                                           probe_real, probe_imag, energy_ev,
-                                                           psize_cm, dist_to_source_cm, det_psize_cm,
-                                                           theta_max, phi_max, free_prop_cm,
-                                                           obj_batch_shape=obj_rot_batch.shape[:-1])
-        else:
-            exiting = multislice_propagate_batch_numpy(obj_rot_batch[:, :, :, :, 0], obj_rot_batch[:, :, :, :, 1],
-                                                       probe_real, probe_imag, energy_ev,
-                                                       psize_cm, free_prop_cm=free_prop_cm, obj_batch_shape=obj_rot_batch.shape[:-1])
-        return exiting
-
-    # read model
-    grid_delta = np.load(os.path.join(phantom_path, 'grid_delta.npy'))
-    grid_beta = np.load(os.path.join(phantom_path, 'grid_beta.npy'))
-    img_dim = grid_delta.shape
-    obj = np.zeros(np.append(img_dim, 2))
-    obj[:, :, :, 0] = grid_delta
-    obj[:, :, :, 1] = grid_beta
-
-    if probe_type == 'point':
-        dist_to_source_cm = kwargs['dist_to_source_cm']
-        det_psize_cm = kwargs['det_psize_cm']
-        theta_max = kwargs['theta_max']
-        phi_max = kwargs['phi_max']
-
-    # list of angles
-    theta_ls = -np.linspace(theta_st, theta_end, n_theta) / np.pi * 180
-    n_batch = np.ceil(float(n_theta) / batch_size)
-    theta_batch = np.array_split(theta_ls, n_batch)
-
-    # create data file
-    flag_overwrite = 'y'
-    if os.path.exists(os.path.join(save_folder, fname)):
-        flag_overwrite = input('File exists. Overwrite? (y/n) ')
-    if flag_overwrite in ['y', 'Y']:
-        f = h5py.File(os.path.join(save_folder, fname), 'w')
-        grp = f.create_group('exchange')
-        dat = grp.create_dataset('data', shape=(n_theta, grid_delta.shape[0], grid_delta.shape[1]), dtype=np.complex64)
-    else:
-        return
-
-    # create probe function
-    if probe_type == 'plane':
-        probe_real = np.ones([img_dim[0], img_dim[1]], dtype='float32')
-        probe_imag = np.zeros([img_dim[0], img_dim[1]], dtype='float32')
-    elif probe_type == 'fixed':
-        assert wavefront_initial.shape == img_dim
-        probe_mag, probe_phase = wavefront_initial
-        probe_real, probe_imag = mag_phase_to_real_imag(probe_mag, probe_phase)
-    elif probe_type == 'point':
-        probe_real = np.ones([img_dim[0], img_dim[1]], dtype='float32')
-        probe_imag = np.zeros([img_dim[0], img_dim[1]], dtype='float32')
-    elif probe_type == 'gaussian':
-        probe_mag_sigma = kwargs['probe_mag_sigma']
-        probe_phase_sigma = kwargs['probe_phase_sigma']
-        probe_phase_max = kwargs['probe_phase_max']
-        py = np.arange(obj.shape[0]) - (obj.shape[0] - 1.) / 2
-        px = np.arange(obj.shape[1]) - (obj.shape[1] - 1.) / 2
-        pxx, pyy = np.meshgrid(px, py)
-        probe_mag = np.exp(-(pxx ** 2 + pyy ** 2) / (2 * probe_mag_sigma ** 2))
-        probe_phase = probe_phase_max * np.exp(
-            -(pxx ** 2 + pyy ** 2) / (2 * probe_phase_sigma ** 2))
-        probe_real, probe_imag = mag_phase_to_real_imag(probe_mag, probe_phase)
-    else:
-        raise ValueError('Invalid wavefront type. Choose from \'plane\', \'point\', or \'fixed\'.')
-
-    for i_batch, this_theta_batch in tqdm(enumerate(theta_batch)):
-        wave_out = rotate_and_project(this_theta_batch, obj)
-        if monitor_output:
-            dxchange.write_tiff(np.abs(wave_out), os.path.join(save_folder, 'monitor_output', 'prj_{}'.format(i_batch)), dtype='float32', overwrite=True)
-        dat[i_batch * batch_size:i_batch * batch_size + batch_size, :, :] = wave_out
-    f.close()
-    return
-
-
-def create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_path, save_folder, fname, probe_pos,
-                                         probe_type='gaussian', probe_size=(72, 72), wavefront_initial=None,
-                                         theta_st=0, theta_end=2*PI, probe_circ_mask=0.9, minibatch_size=23, fresnel_approx=True,
+def create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_path, save_path, fname, probe_pos,
+                                         probe_type='gaussian', probe_size=(72, 72), probe_initial=None,
+                                         theta_st=0, theta_end=2*PI, minibatch_size=23, fresnel_approx=True,
                                          free_prop_cm='inf', **kwargs):
     """
     If probe_type is 'gaussian', supply parameters 'probe_mag_sigma', 'probe_phase_sigma', 'probe_phase_max'.
@@ -126,7 +41,8 @@ def create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_p
             grid_beta_ls = np.array(grid_beta_ls)
             exiting = multislice_propagate_batch_numpy(grid_delta_ls, grid_beta_ls, probe_real, probe_imag, energy_ev,
                                                        psize_cm, free_prop_cm=free_prop_cm,
-                                                       obj_batch_shape=[len(pos_batch), probe_size[0], probe_size[1], grid_delta.shape[-1]], fresnel_approx=fresnel_approx)
+                                                       obj_batch_shape=[len(pos_batch), probe_size[0], probe_size[1], grid_delta.shape[-1]],
+                                                       fresnel_approx=fresnel_approx)
             if k == 0:
                 exiting_ls = np.copy(exiting)
             else:
@@ -155,27 +71,27 @@ def create_ptychography_data_batch_numpy(energy_ev, psize_cm, n_theta, phantom_p
 
     # create data file
     flag_overwrite = 'y'
-    if os.path.exists(os.path.join(save_folder, fname)):
+    if os.path.exists(os.path.join(save_path, fname)):
         flag_overwrite = input('File exists. Overwrite? (y/n) ')
     if flag_overwrite in ['y', 'Y']:
-        f = h5py.File(os.path.join(save_folder, fname), 'w')
+        f = h5py.File(os.path.join(save_path, fname), 'w')
         grp = f.create_group('exchange')
         dat = grp.create_dataset('data', shape=(n_theta, len(probe_pos), probe_size[0], probe_size[1]), dtype=np.complex64)
     else:
         return
 
-    probe_real, probe_imag = initialize_probe(probe_size, probe_type, probe_initial=wavefront_initial)
+    probe_real, probe_imag = initialize_probe(probe_size, probe_type, probe_initial=probe_initial, **kwargs)
     probe_mag, probe_phase = real_imag_to_mag_phase(probe_real, probe_imag)
 
-    dxchange.write_tiff(probe_mag, os.path.join(save_folder, 'probe_mag'), dtype='float64', overwrite=True)
-    dxchange.write_tiff(probe_phase, os.path.join(save_folder, 'probe_phase'), dtype='float64', overwrite=True)
-    dxchange.write_tiff(probe_mag, os.path.join(save_folder, 'probe_mag_f32'), dtype='float32', overwrite=True)
-    dxchange.write_tiff(probe_phase, os.path.join(save_folder, 'probe_phase_f32'), dtype='float32', overwrite=True)
+    dxchange.write_tiff(probe_mag, os.path.join(save_path, 'probe_mag'), dtype='float64', overwrite=True)
+    dxchange.write_tiff(probe_phase, os.path.join(save_path, 'probe_phase'), dtype='float64', overwrite=True)
+    # dxchange.write_tiff(probe_mag, os.path.join(save_path, 'probe_mag_f32'), dtype='float32', overwrite=True)
+    # dxchange.write_tiff(probe_phase, os.path.join(save_path, 'probe_phase_f32'), dtype='float32', overwrite=True)
 
     for ii, theta in enumerate(theta_ls):
         print('Theta: {}'.format(ii))
         waveset_out = rotate_and_project(theta, obj)
         dat[ii, :, :, :] = waveset_out
-        dxchange.write_tiff(abs(waveset_out), os.path.join(save_folder, 'diffraction_dat', 'mag_{:05d}'.format(ii)), overwrite=True, dtype='float32')
+        dxchange.write_tiff(abs(waveset_out), os.path.join(save_path, 'diffraction_dat', 'mag_{:05d}'.format(ii)), overwrite=True, dtype='float32')
     f.close()
     return
