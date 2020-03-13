f7af42df42e1e7fc1c026ef0dc64ea13e26ac0e9
mdw771 <mingdu2015@u.northwestern.edu>
2020-03-12 20:15:50 -0500

    adam for probe

--- adorym/ptychography.py
+++ adorym/ptychography.py
@@ -325,11 +325,9 @@ def reconstruct_ptychography(
         # ================================================================================
         opt_args_ls = [0, 1]
         if probe_type == 'optimizable':
-            opt_probe = GDOptimizer([*probe_size, 2], output_folder=output_folder)
+            opt_probe = AdamOptimizer([*probe_size, 2], output_folder=output_folder)
             opt_probe.create_param_arrays(device=device_obj)
-            optimizer_options_probe = {'step_size': probe_learning_rate,
-                                      'dynamic_rate': True,
-                                      'first_downrate_iteration': 4 * max([ceil(n_pos / (minibatch_size * n_ranks)), 1])}
+            optimizer_options_probe = {'step_size': probe_learning_rate}
             opt_probe.set_index_in_grad_return(len(opt_args_ls))
             opt_args_ls = opt_args_ls + [2, 3]
 
