Fix inconsistent pre-rotation and runtime rotation

adorym/admm_ptychography.py
@@ -161,14 +161,14 @@ class PhaseRetrievalSubproblem(Subproblem):
         self.probe_size = probe_exit[0].shape[1:]
         self.n_probe_modes = probe_exit[0].shape[0]
         self.prj = prj
-        self.optimizer.create_param_arrays(self.psi_theta_ls.shape, device=self.device)
+        self.optimizer.create_param_arrays(self.psi_theta_ls.shape, device=None)
         self.optimizer.set_index_in_grad_return(0)
         if self.optimize_probe:
             if self.common_probe:
-                self.probe_optimizer.create_param_arrays([*self.probe_real.shape, 2], device=self.device)
+                self.probe_optimizer.create_param_arrays([*self.probe_real.shape, 2], device=None)
             else:
                 self.probe_optimizer.create_param_arrays([self.n_theta, max(self.n_pos_ls), *self.probe_real.shape, 2],
-                                                         device=self.device)
+                                                         device=None)
             self.probe_optimizer.set_index_in_grad_return(0)
 
     def allocate_over_theta(self):
@@ -854,7 +854,7 @@ class AlignmentSubproblem(Subproblem):
         self.lambda1_theta_ls_local = w.zeros([len(self.theta_ind_ls_local), *self.whole_object_size[:2], 2],
                                               device=self.device)
 
-        self.optimizer.create_param_arrays(self.w_theta_ls_local.shape, device=self.device)
+        self.optimizer.create_param_arrays(self.w_theta_ls_local.shape, device=None)
         self.shift_params = w.zeros([self.n_theta, 2], device=self.device)
 
     def update_psi_data_mpi(self):
@@ -1022,7 +1022,8 @@ class AlignmentSubproblem(Subproblem):
 
 class BackpropSubproblem(Subproblem):
     def __init__(self, whole_object_size, binning, energy_ev, psize_cm, safe_zone_width=0,
-                 rho=1., n_tiles_y=1, n_tiles_x=1, optimizer=None, device=None, debug=False, stdout_options={}):
+                 rho=1., n_tiles_y=1, n_tiles_x=1, optimizer=None, device=None, debug=False,
+                 stdout_options={}):
         """
         Alignment subproblem solver.
 
@@ -1093,7 +1094,7 @@ class BackpropSubproblem(Subproblem):
             self.save_variable(lmbda2, 'lambda2_{:04d}'.format(i_theta))
 
         self.optimizer.create_param_arrays([self.n_theta_local, *self.tile_shape, self.whole_object_size[2], 2],
-                                           device=self.device)
+                                           device=None)
         self.optimizer.set_index_in_grad_return(0)
 
     def locate_theta_data(self, i_theta):
@@ -1533,7 +1534,8 @@ class TomographySubproblem(Subproblem):
             lmbda3 = np.zeros([*self.whole_object_size, 2])
             self.save_variable(lmbda3, 'lambda3_{:04d}'.format(i_theta))
 
-        self.optimizer.create_distributed_param_arrays(self.x.arr.shape)
+        self.optimizer.distribution_mode = 'distributed_object'
+        self.optimizer.create_container(self.x.arr.shape, False, None)
         self.optimizer.set_index_in_grad_return(0)
 
     def forward(self, x, theta, reverse=False):

adorym/wrappers.py
@@ -152,6 +152,36 @@ def to_numpy(var):
                 return var.cpu().data.numpy()
 
 
+def to_cpu(var):
+    if isinstance(var, np.ndarray):
+        return var
+    elif isinstance(var, np.float64):
+        return var
+    else:
+        if global_settings.backend == 'autograd':
+            return var
+        elif global_settings.backend == 'pytorch':
+            if var.device.type == 'cpu':
+                return var
+            else:
+                return var.cpu()
+
+
+def to_gpu(var, device='cuda:0'):
+    if isinstance(var, np.ndarray):
+        return var
+    elif isinstance(var, np.float64):
+        return var
+    else:
+        if global_settings.backend == 'autograd':
+            return var
+        elif global_settings.backend == 'pytorch':
+            if var.device.type == 'cuda':
+                return var
+            else:
+                return var.cuda(device=device)
+
+
 def get_device(index=None):
     """
     Get device object.
@@ -169,6 +199,13 @@ def get_var_device(var):
         return None
     elif global_settings.backend == 'pytorch':
         return var.device
+
+
+def get_var_device_type(var):
+    if global_settings.backend == 'autograd':
+        return 'cpu'
+    elif global_settings.backend == 'pytorch':
+        return var.device.type
 
 
 def set_device(device):
@@ -954,13 +991,17 @@ def grid_sample(arr, grid, interpolation='bilinear', axis=0, device=None):
         axis_arrangement[2], axis_arrangement[3] = axis_arrangement[3], axis_arrangement[2]
     arr = permute_axes(arr, axis_arrangement, override_backend='pytorch')
 
-    # Convert grid to [0, 1] scale.
-    arr_center = (tc.tensor(arr.shape[2:4], requires_grad=False, device=device) - 1) / 2
-    grid = (grid - arr_center) / (arr_center + 0.5)
+    # Convert grid to [-1, 1] scale.
+    # arr_center = (tc.tensor(arr.shape[2:4], requires_grad=False, device=device) - 1) / 2
+    # grid = (grid - arr_center) / (arr_center + 0.5)
+    arr_shape = create_constant(arr.shape[2:], dtype=pytorch_dtype_query_mapping_dict[arr.dtype],
+                                device=get_var_device(arr))
+    grid = -1 + 2. * grid / arr_shape + 1. / arr_shape
+
     grid = reshape(grid, [1, *arr.shape[2:4], 2], override_backend='pytorch')
     grid = tile(grid, [arr.shape[0], 1, 1, 1], override_backend='pytorch')
     grid = cast(grid, pytorch_dtype_query_mapping_dict[arr.dtype], override_backend='pytorch')
-    arr = tc.nn.functional.grid_sample(arr, grid, padding_mode='border', mode=interpolation)
+    arr = tc.nn.functional.grid_sample(arr, grid, padding_mode='border', mode=interpolation, align_corners=False)
     arr = permute_axes(arr, [axis_arrangement.index(0), axis_arrangement.index(1),
                              axis_arrangement.index(2), axis_arrangement.index(3)], override_backend='pytorch')
     if flag_convert_arr:

tools/create_ptycho_data.py
@@ -136,7 +136,7 @@ try:
 except: pass
 
 try:
-    params_cone_marc = {'fname': 'data_cone_256_foam_1nm_fine.h5',
+    params_cone_marc = {'fname': 'data_cone_256_foam_1nm_fine_2.h5',
                         'theta_st': 0,
                         'theta_end': 2 * np.pi,
                         'n_theta': 500,